a330c8a8cdd5cf75906fef6de15805b9cfdeca0e
sakibdeveloper999 <arjunsakib43@gmail.com>
2025-10-11 20:44:08 +0600

    first commit

---
+++ React_Introduction/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello React!</h1>
+      <p>Welcome to your first React application.</p>
+    </div>
+  )
+}
+
+export default App
