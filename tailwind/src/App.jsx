51c64cc215bd33df42b189cf63b47d29437ba318
sakibdeveloper999 <arjunsakib43@gmail.com>
2025-10-16 22:31:08 +0600

    update tailwind CSS

--- tailwind/src/App.jsx
+++ tailwind/src/App.jsx
@@ -1,34 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='  bg-gray-900 h-screen w-screen'>
+      <div className="w-200 absolute h-100 bg-red-500 mt-20 ml-20" >
+        <div className="absolute h-60 w-60 rounded-full bg-green-500 mt-23 ml-70 "></div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <h1 className='font-serif text-2xl text-pink-600 pt-10 pl-20'>This flag is a representation of the my country</h1>
+    </div>
   )
 }
 
