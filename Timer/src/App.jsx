590da1a4d424539cff92aeb29772dfd6d63bf748
sakibdeveloper999 <arjunsakib43@gmail.com>
2025-10-24 20:29:39 +0600

    gh

---
+++ Timer/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+  
+  return (
+    <div>
+      <h1>Timer</h1>
+      <p>00:00</p>
+      <button>Start</button>
+      <button>Stop</button>
+      <button>Reset</button>
+
+    </div>
+  )
+}
+
+export default App
