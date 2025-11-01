d1f15f8f463276cc028adb4f58130afc32486753
sakibdeveloper999 <arjunsakib43@gmail.com>
2025-11-01 13:53:34 +0600

    UI add

--- Note App project/src/App.jsx
+++ Note App project/src/App.jsx
@@ -9,13 +9,32 @@ const App = () => {
   }
   return (
     <>
-    <div >
+    <div className='h-screen lg:flex bg-black text-white' >
       <form onSubmit={(e)=>{
         submitHandler(e)
-      }} >
-        <input type="text" placeholder='Title' />
-        <button>Submit</button>
+      }} className='flex gap-4 lg:w-1/2 p-10 flex-col items-start' >
+        <h1 className='text-4xl mb-2 font-bold'>Add note</h1>
+        <input 
+        type="text" 
+        placeholder='Enter Note Heading' 
+        className='px-5 w-full font-medium py-2 border-2 outline-none rounded'
+        />
+        <textarea 
+        type="text"
+        className='px-5 w-full font-medium h-32 py-2 flex items-start flex-row border-2 outline-none rounded'
+        placeholder='Write your note here...'
+        /> 
+        <button className='bg-white font-medium w-full outline-none text-black px-5 py-2 rounded'>
+          Submit
+        </button>
       </form>
+      <div className='lg:w-1/2 p-10 lg:border-l-2 bg-black'>
+         <h1 className='text-4xl font-bold'>Recent Note</h1>
+         <div className="flex flex-wrap gap-5 mt-6 h-full overflow-auto">
+          <div className="h-52 w-40 rounded-xl bg-white "></div>
+          <div className="h-52 w-40 rounded-xl bg-white "></div>
+         </div>
+      </div> 
     </div>
     </>
   )
