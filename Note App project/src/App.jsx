464fbd90776243d7891add4b10c665247c47b990
sakibdeveloper999 <arjunsakib43@gmail.com>
2025-11-12 14:30:50 +0600

    note delete func add

--- Note App project/src/App.jsx
+++ Note App project/src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { Trash2 } from 'lucide-react';
 
 const App = () => {
   const [title, setTitle] = useState('');
@@ -13,8 +14,14 @@ const App = () => {
     console.log(copyTask);
     setTitle('')
     setDetails('')
+    
   }
 
+  const noteDelete=(idx)=>{
+    const dltTask = [...task];
+    dltTask.splice(idx, 1);
+    setTask(dltTask);
+  }
 
 
 
@@ -29,6 +36,7 @@ const App = () => {
           <input
             type="text"
             placeholder='Enter Note Heading'
+            required
             className='px-5 w-full font-medium py-2 border-2 outline-none rounded'
             value={title}
             onChange={(e) => {
@@ -40,6 +48,7 @@ const App = () => {
             type="text"
             className='px-5 w-full font-medium h-32 py-2 flex items-start flex-row border-2 outline-none rounded'
             placeholder='Write your note here...'
+            required
             value={details}
             onChange={(e) => {
               setDetails(e.target.value);
@@ -54,12 +63,15 @@ const App = () => {
           <div className="flex flex-wrap items-start justify-start gap-5 mt-6 h-full overflow-auto">
             {task.map(function (note, idx) {
 
-              return <div key={idx} className="h-52 w-40 rounded-xl bg-white text-black p-2">
-                <div className='flex items-center gap-4 mb-2'>
-                  <h3 className='font-bold'>{note.title}</h3>
-                  <div className='h-2 w-2 bg-black ml-22'></div>
+              return <div key={idx} className="relative h-52 w-40 rounded-xl bg-cover bg-[url('https://static.vecteezy.com/system/resources/previews/037/152/677/non_2x/sticky-note-paper-background-free-png.png')] text-black p-2">
+                <div className='  gap-4 mb-2 flex flex-col items-start'>
+                  <h3 className='font-bold pt-4 uppercase'>{note.title}</h3>
+                  <p className='font-medium text-sm '>{note.details}</p>
+                  <h2 onClick={()=>{
+                    alert('Note Deleted Successfully')
+                    noteDelete(idx)
+                  }} ><Trash2 className='h-6 w-6 bottom-3 absolute left-16 active:scale-90 text-red-500 cursor-pointer' /></h2>
                 </div>
-                <p className='font-medium text-sm '>{note.details}</p>
               </div>
             })}
 
