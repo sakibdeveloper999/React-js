5087f6ccfbb0bf0d607bd0e11661334d4d4597f3
sakibdeveloper999 <arjunsakib43@gmail.com>
2025-11-27 15:39:50 +0600

    How to work useEffect

--- useEffect/src/App.jsx
+++ useEffect/src/App.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
+  const [numOne, setNumOne] = useState(0)
+  const [numTow, setNumTow] = useState(0)
+
+
+  useEffect(() => {
+    console.log(' num one is puls ');
+  }, [numOne]);
+
+  useEffect(() => {
+    console.log(' num tow is puls ');
+  }, [numTow]);
+
+
+  
   return (
-    <div>App</div>
+    <div className='h-20 w-60 m-auto '>
+      <h1 className='text-2xl  text-red-500 mt-10'>Num one is: {numOne}</h1>
+      <h1 className='text-2xl  text-green-500 '>Num tow is: {numTow}</h1>
+      <button 
+      className='px-2 py-3 rounded-2xl mt-20 text-bold bg-red-500 text-white' 
+      onClick={
+        ()=>{setNumOne(numOne +1)}
+        }>Num One
+        </button>
+      <button 
+      className='px-2 py-3  rounded-2xl ml-2 text-bold bg-green-500 text-white' 
+      onClick={
+        ()=>{setNumTow(numTow +1)}
+        }>Num Tow
+        </button>
+    </div>
   )
 }
 
